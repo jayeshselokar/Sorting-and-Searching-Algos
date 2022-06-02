08946832e221748fd7d295bb63fcd983dc6450fc
unknown <jay.selokar@gmail.com>
2022-06-02 12:15:21 +0530

    first commit

---
+++ Linear_n_Binary_search.py
@@ -0,0 +1,5 @@
+# Linear Search
+
+# What is Linear Search?
+
+# A linear search is the simplest method of searching a data set. Starting at the beginning of the data set, each item of data is examined until a match is made. Once the item is found, the search ends.
